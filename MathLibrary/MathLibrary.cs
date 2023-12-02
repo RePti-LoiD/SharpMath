5c236f09e341b7d285b439cba0ad2973dbd6a3b2
Сергей <95445727+RePti-LoiD@users.noreply.github.com>
2023-12-02 21:57:29 +0300

    Добавьте файлы проекта.

---
+++ MathLibrary/MathLibrary.cs
@@ -0,0 +1,12 @@
+﻿namespace MathLibrary;
+
+public class MathLibrary
+{
+    public double Div(double a, double b)
+    {
+        if (a == 0 && b == 0) return 0;
+        else if (b == 0) throw new DivideByZeroException();
+        
+        return a / b;
+    }
+}
---
+++ MathLibraryTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace MathLibraryTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Console.WriteLine("Test started");
+    }
+
+    [Test]
+    public void Test1()
+    {
+
+
+        Assert.Pass();
+    }
+}
